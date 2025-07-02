5562e2339f42ae65509ec7b3f92c49f5062818de
Alberto García <josebetomex@gmail.com>
2025-07-01 18:26:13 -0600

    rename dir for vercel

--- api/index.py
+++ api/index.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+#from flask_cors import CORS
 from agent import DebateAgent
-
+ 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 
 
 @app.route("/", methods=["GET"])
